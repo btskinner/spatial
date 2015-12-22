eb270f762304735d55c94c92e88de003be5388f0
btskinner <b.skinner@vanderbilt.edu>
2015-12-22 13:00:24 -0600

    converted to python 3

--- scripts/neighborcounties.py
+++ scripts/neighborcounties.py
@@ -1,44 +1,118 @@
-
+# ==============================================================================
+#
 # FILE: neighborcounties.py
 # AUTH: Benjamin Skinner
 # INIT: 3 July 2015
+#
+# ==============================================================================
 
 # libraries
 import pysal as ps
 import pandas as pd
+import numpy as np
 
 # data dirs
-shp = '<dir>/tl_2013_us_county.shp'
-dbf = '<dir>/tl_2013_us_county.dbf'
+shp = '../data/tl_2010_us_county10.shp'
+dbf = '../data/tl_2010_us_county10.dbf'
 
-# read neighbors
-print '\nfinding adjacent counties\n'
+# --------------------------------------------------------------------
+# Store neighboring counties
+# --------------------------------------------------------------------
+
+# message
+print('\nFinding adjacent counties.\n')
+
+# read in data, finding counties that share borders
 counties = ps.rook_from_shapefile(shp)
 
-# get accompanying db information
+# store neighbors dictionary
+neighbors = counties.neighbors
+
+# convert dict to dataframe
+neighbors = pd.DataFrame.from_dict(neighbors, orient='index')
+
+# id value is the index value
+neighbors['id'] = neighbors.index
+
+# convert from wide to long
+neighbors = pd.melt(neighbors, id_vars='id', value_name='adjid')
+
+# drop number of neighboring counties
+neighbors = neighbors.drop('variable', axis=1)
+
+# drop values with NaN (flotsam from melt)
+neighbors = neighbors[np.isfinite(neighbors['adjid'])]
+
+# sort by ids
+neighbors = neighbors.sort_values(['id','adjid'])
+
+# --------------------------------------------------------------------
+# Create concordance dataframe
+# --------------------------------------------------------------------
+
+# message
+print('\nCreating concordance dataframe.\n')
+
+# get accompanying database information
 db = ps.open(dbf)
 
-# get origin fips in order
-dbfipslist = [l for l in db[:,3]]
+# select fips column
+concordance = pd.DataFrame(db.by_col_array(['GEOID10']), columns=['fips'])
 
-# init list
-nbrs = []
+# create id for merge
+concordance['id'] = concordance.index
 
-# loop through each county
-print '\nstoring adjacent counties\n'
-for c in range(0, counties.n):
+# --------------------------------------------------------------------
+# Merge to convert ids to fips codes
+# --------------------------------------------------------------------
 
-    # get origin county fips
-    originfips = int(dbfipslist[c])
+# message
+print('\nConverting IDs to FIPS codes.\n')
 
-    # get fips for neighboring counties
-    for n in counties.neighbors[c]:
-        adj = int(db[n,3][0])
-        nbrs.append((originfips, adj))
+# merge to get origin fips values
+df = pd.merge(neighbors, concordance, how='left', left_on='id', right_on='id')
+df = df.rename(columns = {'fips': 'orgfips'})
 
-# place in data frame, sort, and write
-print '\nplacing in dataframe and writing to disk\n'
-df = pd.DataFrame(nbrs, columns = ['originfips', 'adjacentfips'])
-df = df.sort(['originfips','adjacentfips'])
-df.to_csv('../data/neighborcounties.csv', index = False)
+# merge to get adjacent fips values
+df = pd.merge(df, concordance, how='left', left_on='adjid', right_on='id')
+df = df.rename(columns = {'fips': 'adjfips'})
+
+# subset to just origin and adjacent fips values; sort
+df = df[['orgfips','adjfips']]
+df = df.sort_values(['orgfips', 'adjfips'])
+
+# --------------------------------------------------------------------
+# Create indicator for same state counties
+# --------------------------------------------------------------------
+
+# message
+print('\nCreating indicator variable for same state counties.\n')
+
+# convert to floats
+df = df[['orgfips', 'adjfips']].astype(float)
+
+# add indicator for county in same state
+df['instate'] = (np.floor(df['orgfips']/1000)==np.floor((df['adjfips']/1000)))
+df['instate'] = df['instate'].astype(int)
+
+# convert back to string, adding leading zeros
+df['orgfips'] = df['orgfips'].astype(int).astype(str).str.zfill(5)
+df['adjfips'] = df['adjfips'].astype(int).astype(str).str.zfill(5)
+
+# --------------------------------------------------------------------
+# Write to disk
+# --------------------------------------------------------------------
+
+# message
+print('\nWriting to disk.\n')
+
+# final sort
+df = df.sort_values(['orgfips', 'adjfips'])
+
+# write to csv
+df.to_csv('../data/neighborcounties.csv', index=False)
+
+# --------------------------------------------------------------------
+# End file
+# ====================================================================
 
